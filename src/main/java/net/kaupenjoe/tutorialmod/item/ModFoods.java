b9af427fdd4e354e74818cd352868129109781cc
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-01-12 14:51:36 +0100

    food

---
+++ src/main/java/net/kaupenjoe/tutorialmod/item/ModFoods.java
@@ -0,0 +1,7 @@
+package net.kaupenjoe.tutorialmod.item;
+
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFoods {
+    public static final FoodProperties CUCUMBER = (new FoodProperties.Builder()).fast().nutrition(2).saturationMod(0.2F).build();
+}
--- src/main/java/net/kaupenjoe/tutorialmod/item/ModItems.java
+++ src/main/java/net/kaupenjoe/tutorialmod/item/ModItems.java
@@ -21,6 +21,9 @@ public class ModItems {
     public static final RegistryObject<Item> DOWSING_ROD = ITEMS.register("dowsing_rod",
             () -> new DowsingRodItem(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB).durability(16)));
 
+    public static final RegistryObject<Item> CUCUMBER = ITEMS.register("cucumber",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB).food(ModFoods.CUCUMBER)));
+
 
 
     public static void register(IEventBus eventBus) {
