7bf4630170076fe931cf8c4bb7540923d9c31c80
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-01-23 14:46:06 +0100

    animated textures

--- src/main/java/net/kaupenjoe/tutorialmod/item/ModItems.java
+++ src/main/java/net/kaupenjoe/tutorialmod/item/ModItems.java
@@ -60,6 +60,9 @@ public class ModItems {
             () -> new ArmorItem(ModArmorMaterials.CITRINE, EquipmentSlot.FEET,
                     new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
 
+    public static final RegistryObject<Item> MAGIC_DUST = ITEMS.register("magic_dust",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
 
 
     public static void register(IEventBus eventBus) {
