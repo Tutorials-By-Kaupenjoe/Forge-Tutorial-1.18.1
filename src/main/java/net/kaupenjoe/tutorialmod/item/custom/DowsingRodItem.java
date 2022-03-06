e488c82885c072456c28f1e4c66a67d11d4873d7
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-03-06 10:18:25 +0100

    update to 1.18.2

--- src/main/java/net/kaupenjoe/tutorialmod/item/custom/DowsingRodItem.java
+++ src/main/java/net/kaupenjoe/tutorialmod/item/custom/DowsingRodItem.java
@@ -6,6 +6,7 @@ import net.kaupenjoe.tutorialmod.util.InventoryUtil;
 import net.kaupenjoe.tutorialmod.util.ModTags;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
@@ -92,6 +93,6 @@ public class DowsingRodItem extends Item {
     }
 
     private boolean isValuableBlock(Block block) {
-        return ModTags.Blocks.DOWSING_ROD_VALUABLES.contains(block);
+        return Registry.BLOCK.getHolderOrThrow(Registry.BLOCK.getResourceKey(block).get()).is(ModTags.Blocks.DOWSING_ROD_VALUABLES);
     }
 }
--- src/main/java/net/kaupenjoe/tutorialmod/util/ModTags.java
+++ src/main/java/net/kaupenjoe/tutorialmod/util/ModTags.java
@@ -4,33 +4,34 @@ import net.kaupenjoe.tutorialmod.TutorialMod;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.Tags;
 
 public class ModTags {
     public static class Blocks {
-        public static final Tags.IOptionalNamedTag<Block> DOWSING_ROD_VALUABLES
+        public static final TagKey<Block> DOWSING_ROD_VALUABLES
                 = tag("dowsing_rod_valuables");
 
-        private static Tags.IOptionalNamedTag<Block> tag(String name) {
-            return BlockTags.createOptional(new ResourceLocation(TutorialMod.MOD_ID, name));
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation(TutorialMod.MOD_ID, name));
         }
 
-        private static Tags.IOptionalNamedTag<Block> forgeTag(String name) {
-            return BlockTags.createOptional(new ResourceLocation("forge", name));
+        private static TagKey<Block> forgeTag(String name) {
+            return BlockTags.create(new ResourceLocation("forge", name));
         }
     }
 
     public static class Items {
-        public static final Tags.IOptionalNamedTag<Item> CITRINE_GEMS = forgeTag("gems/citrine");
+        public static final TagKey<Item> CITRINE_GEMS = forgeTag("gems/citrine");
 
-        private static Tags.IOptionalNamedTag<Item> tag(String name) {
-            return ItemTags.createOptional(new ResourceLocation(TutorialMod.MOD_ID, name));
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(TutorialMod.MOD_ID, name));
         }
 
-        private static Tags.IOptionalNamedTag<Item> forgeTag(String name) {
-            return ItemTags.createOptional(new ResourceLocation("forge", name));
+        private static TagKey<Item> forgeTag(String name) {
+            return ItemTags.create(new ResourceLocation("forge", name));
         }
     }
 }
