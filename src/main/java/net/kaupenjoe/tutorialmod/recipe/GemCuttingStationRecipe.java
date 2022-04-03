6ca93d7ea97421605c8222f06fb099a3b216dc61
Kaupenjoe <N.Kaupenjohann@gmail.com>
2022-04-03 18:35:32 +0200

    jei compat

--- src/main/java/net/kaupenjoe/tutorialmod/recipe/GemCuttingStationRecipe.java
+++ src/main/java/net/kaupenjoe/tutorialmod/recipe/GemCuttingStationRecipe.java
@@ -32,6 +32,11 @@ public class GemCuttingStationRecipe implements Recipe<SimpleContainer> {
     }
 
     @Override
+    public NonNullList<Ingredient> getIngredients() {
+        return recipeItems;
+    }
+
+    @Override
     public ItemStack assemble(SimpleContainer pContainer) {
         return output;
     }
